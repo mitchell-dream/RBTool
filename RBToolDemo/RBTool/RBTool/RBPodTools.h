a3db723bdee354b5085120a01aa99bd103c8a90f
mengchen <mengchen@roo.com>
2016-10-14 15:40:19 +0800

    add files

--- RBToolDemo/RBTool/RBTool/RBPodTools.h
+++ RBToolDemo/RBTool/RBTool/RBPodTools.h
@@ -1,12 +1,14 @@
 //
-//  NSObject+RBPodTools.h
+//  RBPodTools.h
 //  RBTool
 //
 //  Created by william on 16/10/14.
 //  Copyright © 2016年 Roobo. All rights reserved.
 //
 
-
+#ifndef RBPodTools_h
+#define RBPodTools_h
 #import "SSZipArchive.h"
 
 
+#endif /* RBPodTools_h */
